1b4a94efed75899aa80fad08ab8b99f178ce7c7f
Asutosh Rath <dvnasutosh@gmail.com>
2025-03-20 18:19:17 +0530

    added vector store db functionalities and models and an initial test on ui was done.

--- models/Users.py
+++ models/Users.py
@@ -3,14 +3,11 @@ from uuid import UUID, uuid4
 from beanie import Document, Indexed,Link,init_beanie
 from pydantic import EmailStr, Field
 
-from models.Users import Users
-
-
-
-
-class Session(Document):    
+class Users(Document):
     id:UUID=Field(default_factory=uuid4)
-    user_id:Link[Users]
-    title:str
+    name:str
+    email:Annotated[EmailStr, Indexed(unique=True)]
+    password:str
+    verified:bool=False
+    role:Literal['user','privilaged','admin']='user'
     
-
